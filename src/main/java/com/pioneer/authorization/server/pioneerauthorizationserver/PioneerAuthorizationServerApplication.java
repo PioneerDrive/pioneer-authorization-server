fabbcf473537bfd24a7d8d328b9f6cf523b81b17
Ben Gonzales <bgonzales20001@gmail.com>
2018-12-29 16:53:32 -0600

    Init repo

---
+++ src/main/java/com/pioneer/authorization/server/pioneerauthorizationserver/PioneerAuthorizationServerApplication.java
@@ -0,0 +1,14 @@
+package com.pioneer.authorization.server.pioneerauthorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PioneerAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PioneerAuthorizationServerApplication.class, args);
+	}
+
+}
+
